5cf8c7b525ba4bb68b882da3bfa242d677a5494e
Sherlock <895602241@qq.com>
2022-06-07 11:54:06 +0800

    6/7

--- src/Select.java
+++ src/Select.java
@@ -2,6 +2,7 @@ import java.io.FileNotFoundException;
 import java.io.PrintStream;
 
 public class Select {
+    //read students' information//
     public static Student[] readLength (String st){
         In in = new In(st);
         int length = in.readInt();
@@ -18,15 +19,18 @@ public class Select {
         In in = new In("choice.txt");
         int length = in.readInt();
         Student[] students = readLength("choice.txt");
+
+        // Randomly sort the array of students//
         RandomSort randomSort1 = new RandomSort(students);
         students = randomSort1.shuffle();
 
+        // Output the result into "Test1.txt"//
         PrintStream ps1 = new PrintStream("Test1.txt");
         System.setOut(ps1);
         for (int i = 0; i < length; i ++) {
             System.out.println(students[i].name + " " + students[i].allocated);
         }
-
+        int maxNum = 4;
         int place1Num = 0;
         int place2Num = 0;
         int place3Num = 0;
@@ -37,32 +41,32 @@ public class Select {
         //first allocation//
         for (int i = 0; i < length; i++) {
             if (students[i].allocated == null) {
-                if (students[i].firstChoice == 1 && place1Num < 6) {
+                if (students[i].firstChoice == 1 && place1Num < maxNum) {
                     place1Num++;
                     students[i].allocated = "浙江碧扬";
                     continue;
                 }
-                if (students[i].firstChoice == 2 && place2Num < 6) {
+                if (students[i].firstChoice == 2 && place2Num < maxNum) {
                     place2Num++;
                     students[i].allocated = "环科院土壤所";
                     continue;
                 }
-                if (students[i].firstChoice == 3 && place3Num < 6) {
+                if (students[i].firstChoice == 3 && place3Num < maxNum) {
                     place3Num++;
                     students[i].allocated = "环科院规划所";
                     continue;
                 }
-                if (students[i].firstChoice == 4 && place4Num < 6) {
+                if (students[i].firstChoice == 4 && place4Num < maxNum) {
                     place4Num++;
                     students[i].allocated = "监测中心";
                     continue;
                 }
-                if (students[i].firstChoice == 5 && place5Num < 6) {
+                if (students[i].firstChoice == 5 && place5Num < maxNum) {
                     place5Num++;
                     students[i].allocated = "绍兴深水";
                     continue;
                 }
-                if (students[i].firstChoice == 6 && place6Num < 6) {
+                if (students[i].firstChoice == 6 && place6Num < maxNum) {
                     place6Num++;
                     students[i].allocated = "桃花源";
                     continue;
@@ -83,32 +87,32 @@ public class Select {
         //second allocation//
         for (int i = 0; i < length; i++) {
             if (students[i].allocated == null) {
-                if (students[i].secondChoice == 1 && place1Num < 6) {
+                if (students[i].secondChoice == 1 && place1Num < maxNum) {
                     place1Num++;
                     students[i].allocated = "浙江碧扬";
                     continue;
                 }
-                if (students[i].secondChoice == 2 && place2Num < 6) {
+                if (students[i].secondChoice == 2 && place2Num < maxNum) {
                     place2Num++;
                     students[i].allocated = "环科院土壤所";
                     continue;
                 }
-                if (students[i].secondChoice == 3 && place3Num < 6) {
+                if (students[i].secondChoice == 3 && place3Num < maxNum) {
                     place3Num++;
                     students[i].allocated = "环科院规划所";
                     continue;
                 }
-                if (students[i].secondChoice == 4 && place4Num < 6) {
+                if (students[i].secondChoice == 4 && place4Num < maxNum) {
                     place4Num++;
                     students[i].allocated = "监测中心";
                     continue;
                 }
-                if (students[i].secondChoice == 5 && place5Num < 6) {
+                if (students[i].secondChoice == 5 && place5Num < maxNum) {
                     place5Num++;
                     students[i].allocated = "绍兴深水";
                     continue;
                 }
-                if (students[i].secondChoice == 6 && place6Num < 6) {
+                if (students[i].secondChoice == 6 && place6Num < maxNum) {
                     place6Num++;
                     students[i].allocated = "桃花源";
                     continue;
@@ -129,32 +133,32 @@ public class Select {
         //third allocation//
         for (int i = 0; i < length; i++) {
             if (students[i].allocated == null) {
-                if (students[i].thirdChoice == 1 && place1Num < 6) {
+                if (students[i].thirdChoice == 1 && place1Num < maxNum) {
                     place1Num++;
                     students[i].allocated = "浙江碧扬";
                     continue;
                 }
-                if (students[i].thirdChoice == 2 && place2Num < 6) {
+                if (students[i].thirdChoice == 2 && place2Num < maxNum) {
                     place2Num++;
                     students[i].allocated = "环科院土壤所";
                     continue;
                 }
-                if (students[i].thirdChoice == 3 && place3Num < 6) {
+                if (students[i].thirdChoice == 3 && place3Num < maxNum) {
                     place3Num++;
                     students[i].allocated = "环科院规划所";
                     continue;
                 }
-                if (students[i].thirdChoice == 4 && place4Num < 6) {
+                if (students[i].thirdChoice == 4 && place4Num < maxNum) {
                     place4Num++;
                     students[i].allocated = "监测中心";
                     continue;
                 }
-                if (students[i].thirdChoice == 5 && place5Num < 6) {
+                if (students[i].thirdChoice == 5 && place5Num < maxNum) {
                     place5Num++;
                     students[i].allocated = "绍兴深水";
                     continue;
                 }
-                if (students[i].thirdChoice == 6 && place6Num < 6) {
+                if (students[i].thirdChoice == 6 && place6Num < maxNum) {
                     place6Num++;
                     students[i].allocated = "桃花源";
                     continue;
@@ -167,5 +171,12 @@ public class Select {
         for (int i = 0; i < length; i ++) {
             System.out.println(students[i].name + " " + students[i].allocated);
         }
+        System.out.println();
+        System.out.println("浙江碧扬" + "： " + place1Num);
+        System.out.println("环科院土壤所" + "： " + place2Num);
+        System.out.println("浙江规划所" + "： " + place3Num);
+        System.out.println("监测中心" + "： " + place4Num);
+        System.out.println("绍兴深水" + "： " + place5Num);
+        System.out.println("桃花源" + "： " + place6Num);
     }
 }
